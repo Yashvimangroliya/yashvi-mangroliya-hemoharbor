a0c2836f23a6dc4b69b79327cad3f67951391265
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-02-19 15:14:05 -0500

    intial commit

---
+++ src/pages/auth/Login.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Login() {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-8 form-banner">
+          <img
+            src="./assets/images/banner1.jpg"
+            alt="background for home page"
+          />
+        </div>
+        <div className="col-md-4 form-container">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
