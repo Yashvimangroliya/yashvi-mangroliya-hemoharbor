3e27dff342b9f8e189ce9342686f836fd0451d8c
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-02-22 22:15:01 -0500

    add protected

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./components/Routes/ProtectedRoute";
 function App() {
   return (
     <>
-    <ToastContainer/>
+      <ToastContainer />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
